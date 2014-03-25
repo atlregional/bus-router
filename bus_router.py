354321ad6f7ca64fd430e89f37e6260b2381a6f0
Landon Reed <landontreed@gmail.com>
2014-03-25 11:10:08 -0400

    dear god, I'm sorry this is so messy and that I didn't just use a database

--- bus_router.py
+++ bus_router.py
@@ -1,6 +1,83 @@
-import json
+import csv, os, subprocess, json
+
+class AutoVivification(dict):
+    """Implementation of perl's autovivification feature."""
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
 
 def main():
+	
+
+	datadir = os.path.join(os.getcwd(), 'data')
+	stopspath = os.path.join(datadir, 'stops.txt')
+	timespath = os.path.join(datadir, 'stop_times.txt')
+	tripspath = os.path.join(datadir, 'trips.txt')
+	trips = AutoVivification()
+	names = {}
+	with open(tripspath, 'rb') as tripsfile:
+		tripsreader = csv.DictReader(tripsfile)
+		for trip in tripsreader:
+			
+			tripname = trip['route_id'] + trip['trip_headsign']
+			if tripname in names:
+				break
+			else:
+				names[trip['trip_id']] = tripname
+				print tripname
+			
+	# read times into dict
+	with open(timespath, 'rb') as timesfile:
+		timesreader = csv.DictReader(timesfile)
+		tripscount = 0
+		stopscount = 0
+		for time in timesreader:
+			tripname = names[time['trip_id']]
+			if stopscount == 0 and not (tripname in trips):
+				trips[tripname]['stops'][stopscount]['id'] = time["stop_id"]
+				stopscount += 1
+				curr_trip = tripname
+				tripscount += 1
+
+			elif tripname == curr_trip:
+				trips[tripname]['stops'][stopscount]['id'] = time["stop_id"]
+				stopscount += 1
+				curr_trip = tripname
+
+			else:
+				stopscount = 0
+	print trips
+	# read stops into dict
+	with open(stopspath, 'rb') as stopsfile:
+		stopsreader = csv.DictReader(stopsfile)
+		for stop in stopsreader:
+			print stop['stop_id']
+			for trip in trips:
+				print trip
+				for stoptime in trips[trip]['stops']:
+					print trips[trip]['stops'][stoptime]['id']
+					if stop['stop_id'] == trips[trip]['stops'][stoptime]['id']:
+						trips[trip]['stops'][stoptime]['lat'] = stop['stop_lat']
+						trips[trip]['stops'][stoptime]['lon'] = stop['stop_lon']
+	with open('data.txt', 'w') as outfile:
+		json.dump(trips, outfile)
+		
+				
+
+# for trip in trips:
+# 			print trip
+# 			for stop in trips[trip]['stops']:
+# 				print 'id: ' + trips[trip]['stops'][stop]['id']
+# 				for row in stopsreader:
+# 					print row['stop_id'] + ' & ' + trips[trip]['stops'][stop]['id']
+# 					if row['stop_id'] == trips[trip]['stops'][stop]['id']:
+# 						trips[trip]['stops'][stop]['lat'] = row['stop_lat']
+# 						trips[trip]['stops'][stop]['lon'] = row['stop_lon']
+# 						print trips[trip]['stops'][stop]
+
 	# read stop_ids from stop_times.txt
 	# while trip_id is the same, add stops to array inside "tripid" object
 	#[ 
